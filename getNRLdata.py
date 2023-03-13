7fdbff9b43ce2e397106519e99d54561af77eba6
= <=>
2023-03-13 16:21:20 +1000

    Minor amendments to the script

--- getNRLdata.py
+++ getNRLdata.py
@@ -1,13 +1,13 @@
-# url that i use to get my footy results and fixtures from
+# url that i use to get my footy results and fixtures https://fixturedownload.com/download/nrl-2023-UTC.csv
 import requests
 import os
 
 def getNRLdata():
     folder = os.path.dirname(os.path.realpath(__file__))
     print(folder)
-    url = "https://fixturedownload.com/download/nrl-2022-UTC.csv"
+    url = "https://fixturedownload.com/download/nrl-2022-EAustraliaStandardTime.csv"
     print("Fetching data...")
-    r = requests.get(url)
+    r = requests.get(url, verify=True)
     text = r.iter_lines()
     with open(folder + "\\nrl-2022-EAustraliaStandardTime.csv", "w") as f:
         for line in text:
